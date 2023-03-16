1fb42d60e9508a9dcb5d8d0150d7873234133621
Miguel Lorek <mlorek@tenable.com>
2023-03-16 15:28:19 +0000

    example

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,28 @@
-package org.example;public class Main {
+package org.example;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String code = "class C { Module module; Module mOdule; }";
+        CharStream input = CharStreams.fromString(code);
+        org.example.JavaLexer lexer = new org.example.JavaLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        org.example.JavaParser parser = new org.example.JavaParser(tokens);
+        ParseTree tree = parser.compilationUnit();
+        JL jl = new JL();
+        ParseTreeWalker.DEFAULT.walk(jl, tree);
+    }
+}
+
+class JL extends org.example.JavaParserBaseListener {
+    @Override
+    public void enterVariableDeclaratorId(org.example.JavaParser.VariableDeclaratorIdContext ctx) {
+        String s = ctx.identifier().getChild(0).toString();
+        System.out.println(s);
     }
 }
