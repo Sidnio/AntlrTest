e6f082ece8968a910e5bd8530ad9317ec68f5a2b
Sidnio <1396797522@qq.com>
2023-04-09 13:04:13 +0800

    moduleToken

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
@@ -11,8 +12,8 @@ public class Main {
     public static void main(String[] args) {
         String code = "class C { " +
                 "int a;\n" +
-                "Module module\n; " +
-                "Module mOdule\n; }";
+                "Module module2\n; " +
+                "Module module\n; }";
         CharStream input = CharStreams.fromString(code);
         org.example.JavaLexer lexer = new org.example.JavaLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
@@ -24,19 +25,29 @@ public class Main {
 }
 
 class JL extends org.example.JavaParserBaseListener {
+
+
     @Override
     public void enterVariableDeclaratorId(org.example.JavaParser.VariableDeclaratorIdContext ctx) {
-        // String s = ctx.identifier().getChild(0).toString();
-        // System.out.println(s);
-    }
+//        Token token = ctx.identifier().IDENTIFIER().getSymbol();
+//        System.out.println(token.getStartIndex());
+//        System.out.println(token.getStopIndex());
 
+        super.enterVariableDeclaratorId(ctx);
+    }
 
     @Override
     public void enterFieldDeclaration(org.example.JavaParser.FieldDeclarationContext ctx) {
 
-      //  String text = ctx.variableDeclarators().variableDeclarator(0).variableDeclaratorId().identifier().IDENTIFIER().getSymbol().getText();
+        Token token = ctx.variableDeclarators().variableDeclarator(0).variableDeclaratorId().identifier().IDENTIFIER().getSymbol();
+        System.out.println(token.getText());
+        System.out.println(token.getStartIndex());
+        System.out.println(token.getStopIndex());
+        //我这里是 要获取 module   token 的所有参数 而不单是文本数据
 
-        String text = ctx.variableDeclarators().variableDeclarator(0).variableDeclaratorId().getChild(0).getText();
-        System.out.println(text);
+
+
+     /*   String text = ctx.variableDeclarators().variableDeclarator(0).variableDeclaratorId().getChild(0).getText();
+        System.out.println(text);*/
     }
 }
